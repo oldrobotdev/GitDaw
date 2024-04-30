3001125f72fc7ac9f2b99e50f8adb2285f47edd1
Myk <49292718+oldrobotdev@users.noreply.github.com>
2024-04-30 21:01:56 +0200

    Update converter.ts

--- src/converter.ts
+++ src/converter.ts
@@ -8,7 +8,7 @@ export const alsToJson = async (fileName: string): Promise<void> => {
     const fileContent = await fs.readFile(`./${fileName}.als`);
     const unzippedALS = await gunzip(fileContent);
     const json = xml2json(unzippedALS.toString(), { compact: true, spaces: 4 });
-    await fs.writeFile(`./${fileName}`, json);
+    await fs.writeFile(`./${fileName}.json`, json);
   } catch (error: any) {
     throw new Error(
       `An error occurred while converting the Ableton file to JSON. ${error.message}`
@@ -21,7 +21,7 @@ export const jsonToAls = async (fileName: string): Promise<void> => {
     const fileContent = await fs.readFile(`./${fileName}.json`);
     const xml = json2xml(fileContent.toString(), { compact: true, spaces: 4 });
     const zipped = await gzip(xml);
-    await fs.writeFile(`./${fileName}`, zipped);
+    await fs.writeFile(`./${fileName}.als`, zipped);
   } catch (error: any) {
     throw new Error(
       `An error occurred while converting the JSON file to Ableton file. ${error.message}`
