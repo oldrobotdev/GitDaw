b3f50573d7d44f0ce464361ee44333156ac5774c
Myk <49292718+oldrobotdev@users.noreply.github.com>
2024-04-30 20:51:08 +0200

    Update converter.ts

--- src/converter.ts
+++ src/converter.ts
@@ -5,11 +5,10 @@ import { mkdir } from "../utils/file";
 
 export const alsToJson = async (fileName: string): Promise<void> => {
   try {
-    const fileContent = await fs.readFile(`live_set/ableton/${fileName}.als`);
+    const fileContent = await fs.readFile(`./../${fileName}.als`);
     const unzippedALS = await gunzip(fileContent);
     const json = xml2json(unzippedALS.toString(), { compact: true, spaces: 4 });
-    await mkdir("live_set/json");
-    await fs.writeFile(`live_set/json/${fileName}.json`, json);
+    await fs.writeFile(`./../${fileName}`, json);
   } catch (error: any) {
     throw new Error(
       `An error occurred while converting the Ableton file to JSON. ${error.message}`
@@ -19,11 +18,10 @@ export const alsToJson = async (fileName: string): Promise<void> => {
 
 export const jsonToAls = async (fileName: string): Promise<void> => {
   try {
-    const fileContent = await fs.readFile(`live_set/json/${fileName}.json`);
+    const fileContent = await fs.readFile(`./../${fileName}.json`);
     const xml = json2xml(fileContent.toString(), { compact: true, spaces: 4 });
-    await mkdir("live_set/ableton/");
     const zipped = await gzip(xml);
-    await fs.writeFile(`live_set/ableton/${fileName}.als`, zipped);
+    await fs.writeFile(`./../${fileName}`, zipped);
   } catch (error: any) {
     throw new Error(
       `An error occurred while converting the JSON file to Ableton file. ${error.message}`
